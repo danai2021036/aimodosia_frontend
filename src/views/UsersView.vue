13793734eb3a848315e26536a60ee68dd6eed26f
[Danai] <[it2021036@hua.gr]>
2024-01-26 00:25:41 +0200

    New views with user_id

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,14 +1,52 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useApplicationStore } from '@/stores/application.js';
 import { useRemoteData } from '@/composables/useRemoteData.js';
+const router = useRouter();
+const route = useRoute();
+const store = useApplicationStore();
 
+const userIdRef = ref(null);
 const urlRef = ref('http://localhost:9090/admin/users');
 const authRef = ref(true);
 const { data, loading, performRequest } = useRemoteData(urlRef, authRef);
 
 onMounted(() => {
+    userIdRef.value = route.params.id;
     performRequest();
 });
+
+const token = store.userData.accessToken;
+
+const addAimodotis = async () => {
+    try {
+        const response = await fetch(`http://localhost:9090/admin/addroles/${userIdRef.value}/${4}`, {
+        method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+        },
+            body: JSON.stringify({}),
+    });
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data); // Log the response data if needed
+        // Handle success, update UI or perform other actions
+        alert('Added Role Aimodotis!');
+    } else {
+        // Handle error
+        console.error('Error adding role Aimodotis !');
+    }
+} catch (error) {
+    console.error('An unexpected error occurred:', error);
+}
+};
+
+
+const updateId = (id) => {
+    userIdRef.value = id;
+};
 </script>
 
 <template>
@@ -26,6 +64,7 @@ onMounted(() => {
                                 <th>ID</th>
                                 <th>Username</th>
                                 <th>Email</th>
+                                <th>Roles</th>
                             </tr>
                             </thead>
                             <tbody v-if="loading">
@@ -34,19 +73,39 @@ onMounted(() => {
                             </tr>
                             </tbody>
                             <tbody v-if="data">
-                            <tr v-for="user in data">
-                                <td>{{ user.id }}</td>
-                                <td>{{ user.username }}</td>
-                                <td>{{ user.email }}</td>
-                                <!--                                <td>-->
-                                <!--                                    <RouterLink-->
-                                <!--                                        :to="{-->
-                                <!--                                                name: 'bloodtest-details',-->
-                                <!--                                                params: { id: aimodotis.id }-->
-                                <!--                                            }"-->
-                                <!--                                    >Display</RouterLink-->
-                                <!--                                    >-->
-                                <!--                                </td>-->
+                            <tr v-for="user in data" :key="user.id">
+                                <template v-if="user.id!==1">
+                                    <td>{{ user.id }}</td>
+                                    <td>{{ user.username }}</td>
+                                    <td>{{ user.email }}</td>
+                                    <td>{{updateId(user.id)}}</td>
+                                    <td>
+                                        <span v-for="role in user.roles" :key="role.id">
+                                            <span v-if="role.id!==2">
+                                                {{ role.name }}
+                                                , ROLE_USER
+                                            </span>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span v-if="user.roles.some(role => role.id === 3)">
+                                            <b-button @click="addAimodotis" class="btn btn-success">
+                                                Add Role Aimodotis
+                                            </b-button>
+                                            <b-button @click="deleteSecretary" class="btn btn-danger">
+                                                Delete Role Secretary
+                                            </b-button>
+                                        </span>
+                                        <span v-if="user.roles.some(role => role.id === 4)">
+                                            <b-button @click="addSecretary" class="btn btn-success">
+                                                Add Role Secretary
+                                            </b-button>
+                                            <b-button @click="deleteAimodotis" class="btn btn-danger">
+                                                Delete Role Aimodotis
+                                            </b-button>
+                                        </span>
+                                    </td>
+                                </template>
                             </tr>
                             </tbody>
                         </table>
