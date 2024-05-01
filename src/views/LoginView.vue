1a8d9c58207912f8d70b53d9d3afc909a34a65c7
Nafsika <naf.pap2003@gmail.com>
2024-05-01 14:48:17 +0300

    .

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -19,7 +19,7 @@ const onFormSubmit = () => {
     loading.value = true;
     authenticationFailed.value = false;
 
-    fetch('{{backendEnvVar}}'+'/auth/signin', {
+    fetch('api/auth/signin', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
